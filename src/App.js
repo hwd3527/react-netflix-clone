1a3f53a1144f1d1d21f314a232c06a8277fbdcf8
juwon <hwd3527@naver.com>
2023-06-16 22:08:27 +0900

    20230616-2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
+import requests from './api/request';
 import Banner from './components/Banner';
+import Footer from './components/Footer';
 import Nav from './components/Nav';
+import Row from './components/Row';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Nav></Nav>
       <Banner></Banner>
+
+      <Row 
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+     />
+
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
+      <Footer/>
     </div>
   );
 }
