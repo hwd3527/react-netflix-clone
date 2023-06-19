8fa82277abdb5641d26c9bf905978bddc22ecbfc
juwon <hwd3527@naver.com>
2023-06-19 18:46:13 +0900

    20230619-2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
+import { Outlet, Routes, Route } from 'react-router-dom';
 import './App.css';
-import requests from './api/request';
-import Banner from './components/Banner';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
-import Row from './components/Row';
+import MainPage from './pages/MainPage';
+import DetailPage from './pages/DetailPage';
+import SearchPage from './pages/SearchPage';
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav></Nav>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="app">
-      <Nav></Nav>
-      <Banner></Banner>
-
-      <Row 
-        title="NETFLIX ORIGINALS"
-        id="NO"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-     />
-
-      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
-      <Footer/>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import './Nav.css'
+import { useNavigate } from 'react-router-dom';
 
 export default function Nav() {
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener('scroll', () => {
@@ -18,11 +21,33 @@ export default function Nav() {
     }
   }, [])
   
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  }
 
   return (
 	<nav className={`nav ${show && "nav__black"}`}>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/220px-Netflix_2015_logo.svg.png" alt="Netflix Logo" className='nav__logo' onClick={() => window.location.reload()} />
-      <img src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41" alt="User logged" className='nav__avatar' />
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/220px-Netflix_2015_logo.svg.png" 
+        alt="Netflix Logo" 
+        className='nav__logo' 
+        onClick={() => window.location.reload()} 
+      />
+
+      <input 
+        type="text" 
+        value={searchValue} 
+        onChange={handleChange}
+        className='nav__input'
+        placeholder='영화를 검색해주세요'
+      />
+
+      <img 
+        src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41" 
+        alt="User logged" 
+        className='nav__avatar' 
+      />
 	</nav>
   )
 }
