bdba4a1dd0175c7ba0948e5cce8c0dd8ca5606aa
juwon <hwd3527@naver.com>
2023-06-21 10:47:00 +0900

    20230621-2

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
 	baseURL: 'https://api.themoviedb.org/3',
 	params: {
-		api_key: '6a6ad159a38ce24c9a33c8f88f256fa4',
+		api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
 		language: 'ko-KR'
 	}
 });
